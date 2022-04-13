e23205e996f7252792b5b8a1e6c56be0350b0221
Usuario <Usuario@CPD02-M11>
2022-04-13 14:50:00 -0300

    Adicionar arquivos de projeto.

---
+++ AppEtecFlix/AppEtecFlix/Categorias/Acao.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEtecFlix.Categorias
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Acao : ContentPage
+    {
+        public Acao()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppEtecFlix/AppEtecFlix/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppEtecFlix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBatman.Source = ImageSource.FromResource("AppEtecFlix.Posters.batman.png");
+        }
+
+        private void btnBatman_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Navigation.PushAsync(new Categorias.Acao());
+
+            }catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
