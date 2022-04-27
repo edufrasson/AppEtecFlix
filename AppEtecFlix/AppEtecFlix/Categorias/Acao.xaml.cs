237234b2b9e01d71cab816382b0443cb776b7075
edufrasson <edudomine2016@gmail.com>
2022-04-27 15:05:15 -0300

    Adição dos posters dos filmes de ação

--- AppEtecFlix/AppEtecFlix/Categorias/Acao.xaml.cs
+++ AppEtecFlix/AppEtecFlix/Categorias/Acao.xaml.cs
@@ -17,6 +17,55 @@ namespace AppEtecFlix.Categorias
             InitializeComponent();
 
             NavigationPage.SetHasNavigationBar(this, false);
+
+            btnInfiltrado.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.infiltrado.jpg");
+            btnAnonimo.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.anonimo.jpg");
+            btnMateMorra.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.mate_morra.jpg");
+            btnBloodshot.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.bloodshot.jpg");
+            btnResidentEvil.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.resident_evil.jpg");
+            btnBlackout.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.blackout.jpg");
+            btnMatrix.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.matrix.jpg");
+            btnDjango.Source = ImageSource.FromResource("AppEtecFlix.Img.Filmes_Acao.django.jpg");
+        }
+
+        private void btnInfiltrado_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnonimo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMateMorra_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBloodshot_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResidentEvil_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBlackout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMatrix_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDjango_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
